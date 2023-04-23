00d06283802ca7d7ddc71a5d3632012960df6f1b
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-04-23 17:46:40 -0300

    fix: :adhesive_bandage: fixed delete method error

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
+const fs = require("fs");
+const path = require("path");
 const jsonServer = require("json-server");
-const clone = require("clone");
-const data = require("./db.json");
 
-// const isProductionEnv = process.env.NODE_ENV === "production";
 const server = jsonServer.create();
 
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
-// const router = jsonServer.router(isProductionEnv ? clone(data) : "db.json", {
-//   _isFake: isProductionEnv,
-// });
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+const router = jsonServer.router(db);
 
-server.use(middlewares);
-
-// server.use((req, res, next) => {
-//   if (req.path !== "/") router.db.setState(clone(data));
-//   next();
-// });
-
+server.use(jsonServer.defaults());
 server.use(router);
+
 server.listen(process.env.PORT || 8000, () => {
   console.log("JSON Server is running");
 });
